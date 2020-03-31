8fad6c4fe1bfd200009f173eac018f8aa58407aa
Andrzej Korbiel <akorbiel@online.saba.com>
2020-03-31 13:05:35 +0200

    Pug implementation part 2

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,10 @@ import './../styles/main.scss';
 
 const template = require('../templates/index.pug');
 const app = document.getElementById('app');
+const list = require('../templates/list.pug');
 
 const newAnswer = {a : "fine"}
+const values = ['and', 'xand']
 
-app.innerHTML = template(newAnswer.a);
+app.innerHTML = template();
+app.innerHTML += list({values})
