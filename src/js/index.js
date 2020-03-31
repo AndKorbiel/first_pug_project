1245123973375405d7e37d5e8abfae2a2bc3511d
Andrzej Korbiel <akorbiel@online.saba.com>
2020-03-31 16:06:58 +0200

    Pug pt 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,20 @@
 import './../styles/main.scss';
 
 const template = require('../templates/index.pug');
-const app = document.getElementById('app');
+const $app = document.getElementById('app');
+const $menu = document.getElementById('menu');
 const list = require('../templates/list.pug');
+const footer = require('../templates/footer.pug');
+const menuTemplate = require('../templates/menu.pug');
 
 const newAnswer = {a : "fine"}
-const values = ['and', 'xand', 'zzzs']
+export let values = ['and', 'xand', 'zzzs'];
 
-app.innerHTML = template();
-app.innerHTML += list({values})
+if (false) {
+    values = [1,2,3]
+}
+
+
+$menu.innerHTML = menuTemplate();
+
+$app.innerHTML = template({answer: "nnooooo!"}) + list({values}) + footer();
