b37e895b2e674ba6f4c14008994e3dbb15f5d31a
Andrzej Korbiel <akorbiel@online.saba.com>
2020-04-01 14:14:10 +0200

    Additional changes

--- src/js/index.js
+++ src/js/index.js
@@ -4,18 +4,17 @@ const $app = document.getElementById('app');
 const $menu = document.getElementById('menu');
 const $footer = document.getElementById('footer');
 
-const index = require('../templates/index.pug');
-const listPage = require('../templates/list-page.pug');
-const footer = require('../templates/footer.pug');
-const menuTemplate = require('../templates/menu.pug');
+const index = require('../views/index.pug');
+const listPage = require('../views/list-page.pug');
+const footer = require('../views/footer.pug');
+const menuTemplate = require('../views/menu.pug');
 
-// const newAnswer = {a : "fine"}
-// export let values = ['and', 'xand', 'zzzs'];
+const newAnswer = {a : "fine"};
+export let values = ['First value', ' second', ' and so on'];
 
-// if (false) {
-//     values = [1,2,3]
-// }
-
+if (false) {
+    values = [1,2,3]
+}
 
 $menu.innerHTML = menuTemplate();
 $app.innerHTML = index();
